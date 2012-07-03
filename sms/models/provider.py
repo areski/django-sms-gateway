51cd4117367e837301d9dbd251c1ecbe93efe27d
Areski Belaid <areski@gmail.com>
2012-07-03 12:22:49 +0200

    Add SMS Provider + Improve Admin view

---
+++ sms/models/provider.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+from datetime import *
+
+
+class Provider(models.Model):
+    """
+    Provider
+
+    This defines the SMS Provider you want to use to deliver your SMS messages.
+    Each provider will be associated to a Gateway which will link to the API of
+    the Service Provider.
+    """
+    name = models.CharField(unique=True, max_length=255, verbose_name='Name',
+                            help_text=_("Enter Provider Name"))
+    description = models.TextField(verbose_name='Description',
+                               help_text=_("Short description about Provider"))
+    metric = models.IntegerField(default=10, verbose_name='Metric',
+                                 help_text=_("Enter metric in digit"))
+    gateway = models.ForeignKey('sms.Gateway', null=True, blank=True,
+                                help_text=_("Select Gateway"))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Date')
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:        
+        app_label = _('sms')
+        verbose_name = _("Provider")
+        verbose_name_plural = _("Providers")
+
+    def __unicode__(self):
+            return u"%s" % self.name
