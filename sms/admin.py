8bf0c71a8f9b095871159f04adb60fb00e736d9d
Areski Belaid <areski@gmail.com>
2013-12-07 14:42:07 +0100

    Update admin.py

--- sms/admin.py
+++ sms/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.utils.translation import ugettext as _
 from sms.models import Gateway, Message, Reply, Provider
 
